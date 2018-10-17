8313fc2d670cf54b1ff22e10cce73a3fc2d291d5
Vince Andrew Pimentel <contact@vincep.me>
2018-10-17 01:40:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   min_value = 0
   min_key = nil
   name_hash.each do |name, number|
-    if number < min_value
+    if number < min_value || min_value == 0
       min_key = name
-    end 
+    end
   end
   min_key
 end
